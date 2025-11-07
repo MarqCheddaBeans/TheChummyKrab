18e066bcd3397263cb81b3cf0a7a501b213faf32
MarqCheddaBeans <isaiahmv27@yahoo.com>
2025-11-07 17:28:10 -0500

    Add Size enum class to represent constant sizes, including price multiplier and names for sizes

--- src/main/java/com/pluralsight/models/Order.java
+++ src/main/java/com/pluralsight/models/Order.java
@@ -35,37 +35,49 @@ public class Order {
         return items;
     }
 
+    //Override toString from parent class object and formal to look pretty
+    @Override
     public String toString() {
 
+        //Create String summary which we will concat order info
         String summary = "Order summary:\n";
 
+        //Check if customer has anything to order
         if (items.isEmpty()) {
             summary += "You didnt order anything?!\n";
         } else {
+            //cycle through items list and add name and price of each item
             for (MenuItem i : items) {
                 summary += "- " + i.getName() + " $" + String.format("%.2f", i.calculatePrice()) + "\n";
 
                 //We want to add any addons to be displayed
                 if(i instanceof Modify m){
+                    //Create AddOn list to store items implementing Modify( meaning they can have addOns)
                     List<AddOn> addOns = m.getAddOns();
 
+                    //Check if the item implementing Modify actually has any addons
                     if(addOns.isEmpty()){
                         summary += " • No addons\n";
                     }else{
+                        //cycle through addons and add the name
                         for(AddOn a : addOns){
                             summary += "    • " + a.getName();
+
+                            //if add on is premium, add premium tag
                             if( a.isPremium()){
                                 summary += " [Premium]";
                             }
+                            //concat the price of addon
                             summary += "  +$" + String.format("%.2f", a.getPrice()) + "\n";
                         }
                     }
                 }
             }
+            //calculate and concat the total of all addons
             summary += "Total: $" + String.format("%.2f", calculateTotal()) + "\n";
         }
+        //return the variable containing all the items in the order including price , addons and if any are premium
         return summary;
     }
 
     }
-
--- src/main/java/com/pluralsight/models/Size.java
+++ src/main/java/com/pluralsight/models/Size.java
@@ -1,6 +1,49 @@
 package com.pluralsight.models;
 
-//See if we can create an enum class for size since they will be fixed
+//Enumeration (Enum) used to represent group of named constants(ex day of week Mon-Sun will never change)
 public enum Size {
+
+    //Enum constants for our sizes, will multiply price based on size, hold name for our "Burgers"
+
+    //Small will keep base price and give the small burger a name
+    SMALL(1.0, "Jr.Chummy Patty"),
+
+    //Medium will multiply base price by 1.875 and give medium size burger name
+    MEDIUM(1.875,"Chummy Patty"),
+
+    //Large will multiple base price a whopping 2.25x and give large size burger beautiful name
+    LARGE (2.25, "King Neptune's Chummy Patty");
+
+    //We need to set a multiplier and name for each size, name will only be for pattys
+    private final double multiplier;
+    private final String name;
+
+    //Create constructor so Enum class stops yelling at me
+    Size(double multiplier, String name){
+        this.multiplier = multiplier;
+        this.name = name;
+    }
+
+    //Overload constructor so the drinks and side arent named per size
+    Size(double multiplier){
+        this.multiplier = multiplier;
+        this.name = null;
+    }
+
+    //Gotta get them getters
+    public double getMultiplier(){
+        return multiplier;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    //To string to turn SIZE into Size format
+    @Override
+    public String toString(){
+        String lowCase = name().toLowerCase();
+        return Character.toUpperCase(lowCase.charAt(0)) + lowCase.substring(1);
+    }
 
 }
