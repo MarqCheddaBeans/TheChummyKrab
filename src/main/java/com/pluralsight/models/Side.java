5a0d4b48cbb6f1ba12a7d85c7402802d33fffd24
MarqCheddaBeans <isaiahmv27@yahoo.com>
2025-11-07 21:01:54 -0500

    Completed Side class for any sides

--- src/main/java/com/pluralsight/models/Side.java
+++ src/main/java/com/pluralsight/models/Side.java
@@ -4,13 +4,32 @@ package com.pluralsight.models;
 public class Side extends MenuItem{
 
     //The properties of a side
+    private Size size;
 
     //The super constructor
+    public Side(String name, double price, Size size) {
+        super(name, price);
+        this.size = size;
+    }
 
     //Overload the parents methods
+    @Override
+    public double calculatePrice() {
+        return getPrice() * size.getMultiplier();
+    }
 
     //ToString to store Side info for later
+    @Override
+    public String toString(){
+        String info = "======================================================================================================================================================\n";
+        info += getName() + " -  $" + String.format("%.2f", calculatePrice()) + "\n" ;
+        info += "======================================================================================================================================================\n" ;
 
+        return info;
+    }
 
-
+    //getters
+    public Size getSize() {
+        return size;
+    }
 }
